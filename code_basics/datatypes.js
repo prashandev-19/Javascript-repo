3d23e5b359c7c6add0c06084570ade32ae2eb7d1
prashandev-19 <144698239+prashandev-19@users.noreply.github.com>
2024-09-11 18:52:54 +0000

    datatypes

---
+++ code_basics/datatypes.js
@@ -0,0 +1,17 @@
+let name = "Dev Prashan"
+let age = 23
+let is_true = false
+
+//number=> 2 power 53
+//bigint
+//String=> ""
+//boolean=> true or false
+//null=> standalone value has various usecases.(empty != undefined).
+//undefined=> value not assigned.
+//symbol=> unique.
+
+//object
+
+console.log(typeof("Dev")) // typeof returns datatype of value.
+console.log(typeof(null)) // null is of type object.(Special case)
+console.log(typeof(undefined)) // undefined is of type undefined.
