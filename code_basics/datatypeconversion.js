f2162aafb24cf90bd8858d816f7fa5437db56c29
prashandev-19 <144698239+prashandev-19@users.noreply.github.com>
2024-09-12 19:25:56 +0000

    Conversion&Operation

--- code_basics/datatypeconversion.js
+++ code_basics/datatypeconversion.js
@@ -1,9 +1,9 @@
 let score = "33abc"
-console.log(typeof(score))
+//console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)//Nan=>Not a number.
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)//Nan=>Not a number.
 
 // "33" => 33(Converted)
 // "33abc" => NaN
@@ -17,6 +17,37 @@ console.log(valueInNumber)//Nan=>Not a number.
 let someNumber = 33;
 
 let stringconverted = String(someNumber)
-console.log(stringconverted)
-console.log(typeof stringconverted)
+//console.log(stringconverted)
+//console.log(typeof stringconverted)
 //Gets converted in case of String and also data type changes.
+
+
+//**************Operations*************** */
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+ //console.log(2**3)//2 raised to power 3.
+
+ let str1 = "Hello"
+ let str2 = " Dev"
+
+ let str3 = str1 + str2
+//  console.log(str3)
+
+//  console.log("1" + 2 + 2)
+//  console.log(1 + "2")
+//  console.log(1 + 2 + "3")
+
+//  console.log(+true)
+//  console.log(+"")
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2 //Not readable
+
+let gamecounter = 100
+
+console.log(gamecounter++)
+console.log(++gamecounter)
